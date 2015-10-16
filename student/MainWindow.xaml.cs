cc7e39181dc24a387713ab55b83a22f952bb1938
haozhouxu <470001421@qq.com>
2015-10-16 16:17:26 +0800

    新增添加页面

--- student/MainWindow.xaml.cs
+++ student/MainWindow.xaml.cs
@@ -1,18 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+using MySql.Data;
+using MySql.Data.MySqlClient; //引用Mysql.data.dll中的类
+using System.Data;
 namespace student
 {
     /// <summary>
@@ -23,6 +15,70 @@ namespace student
         public MainWindow()
         {
             InitializeComponent();
+
+            string query = "SELECT * FROM student";
+            MySqlConnection myConnection = new MySqlConnection("server=localhost;user=root;password=root;database=xhz1;port=3306;");
+            //MySqlCommand myCommand = new MySqlCommand(query, myConnection);
+            //myConnection.Open();
+            DataSet ds = MySqlHelper.ExecuteDataset(myConnection, query);
+            //MySqlDataReader myDataReader = myCommand.ExecuteReader();
+
+            //新建一个student集合
+            List<Student> StudentList = new List<Student>();
+            foreach (DataTable dt in ds.Tables)
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                    Student stu = new Student(dr["name"].ToString(), dr["number"].ToString());
+                    StudentList.Add(stu);
+                }
+            }
+
+            //新建一个student集合
+            //List<Student> StudentList = null;
+            //while (myDataReader.Read() == true)
+            //{
+            //    Student stu = new Student(myDataReader["name"].ToString(),myDataReader["number"].ToString());
+            //    StudentList.Add(stu);
+            //}
+            //myDataReader.Close();
+            myConnection.Close();
+            
+            this.student.ItemsSource = StudentList;
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    public class Student
+    {
+        public Student(string name,string number)
+        {
+            Name = name;
+            Number = number;
+        }
+
+        private string name; //学生姓名
+        private string number; //学生编号
+        private Image image; //学生头像
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+        public Image Image
+        {
+            get { return image; }
+            set { image = value; }
         }
     }
 }
---
+++ student/SQLHelper/MySQLHelper.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient; //引用Mysql.data.dll中的类
+
+namespace student.SQLHelper
+{
+    class MySQLHelper
+    {
+    //    private static string connectionString = ConfigurationManager.ConnectionStrings["mysqlconn"].ConnectionString;
+    //    /// <summary>
+    //    /// 执行查询语句，返回DataSet
+    //    /// </summary>
+    //    /// <param name="SQLString">查询语句</param>
+    //    /// <returns>DataSet</returns>
+    //    public static DataSet Query(string SQLString)
+    //    {
+    //        using (MySqlConnection connection = new MySqlConnection(connectionString))
+    //        {
+    //            DataSet ds = new DataSet();
+    //            try
+    //            {
+    //                connection.Open();
+    //                MySqlDataAdapter command = new MySqlDataAdapter(SQLString, connection);
+    //                command.Fill(ds);
+    //            }
+    //            catch (System.Data.SqlClient.SqlException ex)
+    //            {
+    //                throw new Exception(ex.Message);
+    //            }
+    //            finally
+    //            {
+    //                connection.Close();
+    //            }
+    //            return ds;
+    //        }
+    //    }
+    //    /// <summary>
+    //    /// 执行SQL语句，返回影响的记录数
+    //    /// </summary>
+    //    /// <param name="SQLString">SQL语句</param>
+    //    /// <returns>影响的记录数</returns>
+    //    public static int ExecuteSql(string SQLString)
+    //    {
+    //        using (MySqlConnection connection = new MySqlConnection(connectionString))
+    //        {
+    //            using (MySqlCommand cmd = new MySqlCommand(SQLString, connection))
+    //            {
+    //                try
+    //                {
+    //                    connection.Open();
+    //                    int rows = cmd.ExecuteNonQuery();
+    //                    return rows;
+    //                }
+    //                catch (System.Data.SqlClient.SqlException e)
+    //                {
+    //                    connection.Close();
+    //                    throw e;
+    //                }
+    //                finally
+    //                {
+    //                    cmd.Dispose();
+    //                    connection.Close();
+    //                }
+    //            }
+    //        }
+    //    }
+    //    /// <summary>
+    //    /// 执行SQL语句，返回影响的记录数
+    //    /// </summary>
+    //    /// <param name="SQLString">SQL语句</param>
+    //    /// <returns>影响的记录数</returns>
+    //    public static int ExecuteSql(string[] arrSql)
+    //    {
+    //        using (MySqlConnection connection = new MySqlConnection(connectionString))
+    //        {
+
+    //            try
+    //            {
+    //                connection.Open();
+    //                MySqlCommand cmdEncoding = new MySqlCommand(SET_ENCODING, connection);
+    //                cmdEncoding.ExecuteNonQuery();
+    //                int rows = 0;
+    //                foreach (string strN in arrSql)
+    //                {
+    //                    using (MySqlCommand cmd = new MySqlCommand(strN, connection))
+    //                    {
+    //                        rows += cmd.ExecuteNonQuery();
+    //                    }
+    //                }
+    //                return rows;
+    //            }
+    //            catch (System.Data.SqlClient.SqlException e)
+    //            {
+    //                connection.Close();
+    //                throw e;
+    //            }
+    //            finally
+    //            {
+    //                connection.Close();
+    //            }
+    //        }
+    //    }
+    }
+}
