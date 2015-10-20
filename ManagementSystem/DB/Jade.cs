1eb57151d275017ac1898995af34f8708e585a66
haozhouxu <470001421@qq.com>
2015-10-20 13:00:03 +0800

    新建数据结构Gold和Jade

---
+++ ManagementSystem/DB/Jade.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ManagementSystem.DB
+{
+    public class Jade : INotifyPropertyChanged
+    {
+        #region 字段
+        private JadeTypeEnum type;
+        private int number;
+        private float weight;
+        private float unitPrice;
+        private float totalPrice;
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region 属性
+        public JadeTypeEnum Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value;
+                NotifyPropertyChanged("Type");
+            }
+        }
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+                NotifyPropertyChanged("Number");
+            }
+        }
+        public float Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                weight = value;
+                NotifyPropertyChanged("Weight");
+            }
+        }
+        public float UnitPrice
+        {
+            get
+            {
+                return unitPrice;
+            }
+            set
+            {
+                unitPrice = value;
+                NotifyPropertyChanged("UnitPrice");
+            }
+        }
+        public float TotalPrice
+        {
+            get
+            {
+                return totalPrice;
+            }
+            set
+            {
+                totalPrice = value;
+                NotifyPropertyChanged("TotalPrice");
+            }
+        }
+        #endregion
+
+        #region 方法
+        Jade(JadeTypeEnum ty, int num,float we,float un)
+        {
+            Type = ty;
+            Number = num;
+            Weight = we;
+            UnitPrice = un;
+            TotalPrice = num * un;
+        }
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+
+    public enum JadeTypeEnum
+    {
+        CircleType = 1,
+        DripType = 2,
+        GemType = 3,
+        PrincessType = 4,
+        HorseEyeType = 5,
+        YellowType = 6,
+        LadderType = 7,
+        JasperType = 8
+    }
+}
---
+++ ManagementSystem/DB/Jewelry.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ManagementSystem.DB
+{
+    public class Jewelry:INotifyPropertyChanged
+    {
+        #region 字段
+        private float weight;
+        private float unitPrice;
+        private float totalPrice;
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region 属性
+        public float Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                weight = value;
+                NotifyPropertyChanged("Weight");
+            }
+        }
+        public float UnitPrice
+        {
+            get
+            {
+                return unitPrice;
+            }
+            set
+            {
+                unitPrice = value;
+                NotifyPropertyChanged("UnitPrice");
+            }
+        }
+        public float TotalPrice
+        {
+            get
+            {
+                return totalPrice;
+            }
+            set
+            {
+                totalPrice = value;
+                NotifyPropertyChanged("TotalPrice");
+            }
+        }
+        #endregion
+
+        #region 方法
+        Gold(float we, float un)
+        {
+            Weight = we;
+            UnitPrice = un;
+            TotalPrice = we * un;
+        }
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+    }
+}
