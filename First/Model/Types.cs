4a47043de45057026a6033d6f39db206ebd07d0c
haozhouxu <470001421@qq.com>
2016-01-17 14:23:36 +0800

    types类型上次没有签入

---
+++ First/Model/Types.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First.Model
+{
+     public class Types : ObservableCollection<TypeEntity>
+    {
+        public Types()
+        {
+            this.Add(new TypeEntity { Type = "耳坠" });
+            this.Add(new TypeEntity { Type = "挂件" });
+            this.Add(new TypeEntity { Type = "戒指" });
+            this.Add(new TypeEntity { Type = "手链" });
+            this.Add(new TypeEntity { Type = "手镯" });
+            string sql = "select * from Type order by strftime('%Y-%m-%d %H%M%S',createtime) desc";
+            string dbFile = "first";
+            using (SQLiteConnection sc1 = new SQLiteConnection(string.Format(SQLiteService.connectionFormat, dbFile)))
+            {
+                SQLiteCommand sCom = new SQLiteCommand(sql, sc1);
+                sc1.Open();
+                using (SQLiteDataReader dr1 = sCom.ExecuteReader())
+                {
+                    while (dr1.Read())
+                    {
+                        TypeEntity te = new TypeEntity();
+                        te.Type = dr1["name"].ToString();
+                        this.Add(te);
+                    }
+                    dr1.Close();
+                }
+                sc1.Close();
+            }
+        }
+    }
+}
