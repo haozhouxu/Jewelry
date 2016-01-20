81bf72b451907a11691e6c86b6f1c280008d9caa
haozhouxu <470001421@qq.com>
2016-01-20 23:53:16 +0800

    把归属人增加到ADD页面里面，Color还没有添加

---
+++ First/Model/ColorTypes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Data.SQLite;
+using First.Helper;
+
+namespace First.Model
+{
+    public class ColorTypes: ObservableCollection<TypeEntity>
+    {
+        public ColorTypes()
+        {
+            string sql = "select * from Type where category='" + GlobalBindingHelper.ColorType + "' order by strftime('%Y-%m-%d %H%M%S',createtime) desc";
+            string dbFile = "first";
+            using (SQLiteConnection sc1 = new SQLiteConnection(string.Format(SQLiteService.connectionFormat, dbFile)))
+            {
+                SQLiteCommand sCom = new SQLiteCommand(sql, sc1);
+                sc1.Open();
+                using (SQLiteDataReader dr1 = sCom.ExecuteReader())
+                {
+                    while (dr1.Read())
+                    {
+                        TypeEntity te = new TypeEntity();
+                        te.Guid = dr1["guid"].ToString();
+                        te.Type = dr1["name"].ToString();
+                        this.Add(te);
+                    }
+                    dr1.Close();
+                }
+                sc1.Close();
+            }
+        }
+    }
+}
---
+++ First/Model/OwnTypes.cs
@@ -0,0 +1,37 @@
+﻿using First.Helper;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First.Model
+{
+    public class OwnTypes : ObservableCollection<TypeEntity>
+    {
+        public OwnTypes()
+        {
+            string sql = "select * from Type where category='" + GlobalBindingHelper.OwnType + "' order by strftime('%Y-%m-%d %H%M%S',createtime) desc";
+            string dbFile = "first";
+            using (SQLiteConnection sc1 = new SQLiteConnection(string.Format(SQLiteService.connectionFormat, dbFile)))
+            {
+                SQLiteCommand sCom = new SQLiteCommand(sql, sc1);
+                sc1.Open();
+                using (SQLiteDataReader dr1 = sCom.ExecuteReader())
+                {
+                    while (dr1.Read())
+                    {
+                        TypeEntity te = new TypeEntity();
+                        te.Guid = dr1["guid"].ToString();
+                        te.Type = dr1["name"].ToString();
+                        this.Add(te);
+                    }
+                    dr1.Close();
+                }
+                sc1.Close();
+            }
+        }
+    }
+}
